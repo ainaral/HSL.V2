d3769884e3a8d5853c7eb757aab525dd923fab91
Ainara <ainaral@metropolia.fi>
2023-03-27 11:52:32 +0300

    Initial Commit

---
+++ HSL.V2/HSL_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  HSL_V2App.swift
+//  HSL.V2
+//
+//  Created by iosdev on 27.3.2023.
+//
+
+import SwiftUI
+
+@main
+struct HSL_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
