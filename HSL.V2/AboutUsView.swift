10b7b53df59a00f6ec399627c8e1449b8f8cb3c9
Ainara <ainara.larranagaflores@metropolia.fi>
2023-03-30 22:40:01 +0300

    added basic setting UI

---
+++ HSL.V2/AboutUsView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutUsView.swift
+//  HSL.V2
+//
+//  Created by iosdev on 30.3.2023.
+//
+
+import SwiftUI
+
+struct AboutUsView: View {
+    var body: some View {
+        Text("Welcome to HSL.V2!")
+    }
+}
+
+struct AboutUsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutUsView()
+    }
+}
