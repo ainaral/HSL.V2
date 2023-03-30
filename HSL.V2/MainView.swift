1c9a82c423efccf35f05536c34aeb0acfa12f716
Ainara <ainara.larranagaflores@metropolia.fi>
2023-03-30 23:31:23 +0300

    added bottom tab navigation and created home and mic views

--- HSL.V2/MainView.swift
+++ HSL.V2/MainView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Welcome to HSL.V2")
+        TabView {
+            HomeView()
+                .tabItem(){
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            MicView()
+                .tabItem(){
+                    Image(systemName: "mic")
+                    Text("Mic")
+                }
+            
+            SettingsView()
+                .tabItem(){
+                    Image(systemName: "gearshape")
+                    Text("Settings")
+                }
         }
-        .padding()
+        .accentColor(.blue)
     }
 }
 
