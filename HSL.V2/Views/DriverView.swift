dfa04c163db0160e557299109280d769ddce5276
sallyzy0123 <sally_zy@protonmail.com>
2023-04-22 14:14:43 +0300

    polyline in driver view works

--- HSL.V2/Views/DriverView.swift
+++ HSL.V2/Views/DriverView.swift
@@ -13,14 +13,16 @@ struct DriverView: View {
     @StateObject private var viewModel = DriverViewModel()
 
     var body: some View {
-        ZStack {
-            mapLayer
-                .ignoresSafeArea()
-
-            VStack {
-                SearchBarView(searchText: $viewModel.searchText)
-                    .padding()
-                Spacer()
+        NavigationStack {
+            ZStack {
+                mapLayer
+                    .ignoresSafeArea()
+                    
+                VStack {
+                    SearchBarView(searchText: $viewModel.searchText)
+                        .padding()
+                    Spacer()
+                }
             }
         }
     }
@@ -45,71 +47,69 @@ extension DriverView {
 struct SearchBarView: View {
     
     @Binding var searchText: String
-    @StateObject private var viewModel = DriverViewModel()
+    @ObservedObject private var viewModel = DriverViewModel()
     
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .opacity(searchText.isEmpty ? 1.0 : 0.0)
-                
-                TextField("Search by your bus number...", text: $searchText)
-                    .disableAutocorrection(true)
-                    .onChange(of: searchText) { searchText in
-                        viewModel.searchText = searchText
-                    }
-                    .overlay(
-                        HStack {
-                            
-                            
-                            Spacer()
-                            
-                            Image(systemName: "magnifyingglass")
-                                .offset(x: 10)
-                                .opacity(searchText.isEmpty ? 0.0 : 1.0)
-                                .onTapGesture {
-                                    viewModel.fetchBusesByNumber(search: searchText)
-                                    viewModel.showBusList.toggle()
-                                }
-                            
-                            Image(systemName: "xmark.circle.fill")
-                                .padding()
-                                .offset(x: 10)
-                                .opacity(searchText.isEmpty ? 0.0 : 1.0)
-                                .onTapGesture {
-                                    UIApplication.shared.endEditing()
-                                    searchText = ""
-                                    viewModel.showBusList = false
-                                }
+            VStack {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .opacity(searchText.isEmpty ? 1.0 : 0.0)
+                    
+                    TextField("Search by your bus number...", text: $searchText)
+                        .disableAutocorrection(true)
+                        .onChange(of: searchText) { searchText in
+                            viewModel.searchText = searchText
                         }
-                        , alignment: .trailing
-                    )
-            }
-            .font(.headline)
-            .padding()
-            .background(
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color.white)
-                    .shadow(
-                        color: Color.black.opacity(0.8),
-                        radius: 10, x: 0, y:0)
-            )
-            .padding(.horizontal)
-            
-            if viewModel.showBusList {
-//                guard let buses = viewModel.buses else {
-                if let buses = viewModel.buses as [Bus]? {
-                    List(buses, id: \.gtfsId) { bus in
-                        Button {
-                            viewModel.fetchRouteByBus(search: bus.shortName)
-                        } label: {
-                            Text(bus.shortName)
-                        }
-                        .padding(.vertical, 4)
-                    }
-                    .listStyle(PlainListStyle())
+                        .overlay(
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .offset(x: 10)
+                                    .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                                    .onTapGesture {
+                                        viewModel.fetchData(queryType: .busesByNumber(search: searchText))
+                                    }
+//                                    .alert(isPresented: $viewModel.showAlert) {
+//                                        Alert(title: Text("No bus"), message: Text("Please check your bus"), dismissButton: .default(Text("OK")))
+//                                    }
+
+                                Image(systemName: "xmark.circle.fill")
+                                    .padding()
+                                    .offset(x: 10)
+                                    .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                                    .onTapGesture {
+                                        UIApplication.shared.endEditing()
+                                        searchText = ""
+                                        viewModel.showBusList = false
+                                    }
+                            }
+                            , alignment: .trailing
+                        )
                 }
-            }
+                .font(.headline)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.white)
+                        .shadow(
+                            color: Color.black.opacity(0.8),
+                            radius: 10, x: 0, y:0)
+                )
+                .padding(.horizontal)
+                
+                
+                if viewModel.showBusList {
+                    if let buses = viewModel.buses as [Bus]? {
+                        List(buses, id: \.gtfsId) { bus in
+                            NavigationLink(
+                                bus.shortName,
+                                destination:
+                                    MapView(busName: bus.shortName)
+                                        .ignoresSafeArea()
+                            )
+                        }
+                        .listStyle(PlainListStyle())
+                    }
+                }
         }
     }
 }
