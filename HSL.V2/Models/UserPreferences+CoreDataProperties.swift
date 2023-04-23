917691f8f9dcad2ef14e1354bce405f82a444829
Ainara Larranaga <ainara.larranagaflores@metropolia.fi>
2023-04-23 15:41:49 +0300

    generate classes

---
+++ HSL.V2/Models/UserPreferences+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  UserPreferences+CoreDataProperties.swift
+//  HSL.V2
+//
+//  Created by iosdev on 23.4.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension UserPreferences {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<UserPreferences> {
+        return NSFetchRequest<UserPreferences>(entityName: "UserPreferences")
+    }
+
+    @NSManaged public var language: String?
+    @NSManaged public var role: String?
+    @NSManaged public var fullName: String?
+
+}
+
+extension UserPreferences : Identifiable {
+
+}
