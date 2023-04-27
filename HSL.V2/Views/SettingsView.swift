881257aa0f07c924de12ab0f8634015f1fdb5be8
Ainara Larranaga <ainara.larranagaflores@metropolia.fi>
2023-04-27 17:45:28 +0300

    Saving, fetching and updating name to the core data works

--- HSL.V2/Views/SettingsView.swift
+++ HSL.V2/Views/SettingsView.swift
@@ -10,81 +10,93 @@ import CoreData
 
 struct SettingsView: View {
     
-    // Reference to managed object context
-    let context = CoreDataManager.shared.persistentContainer.viewContext
+    @StateObject private var viewModel = SettingsViewModel()
     
-    // Data for the information
-    @FetchRequest(entity: UserPreferences.entity(), sortDescriptors: [], animation: .default)
-    var userPreferences: FetchedResults<UserPreferences>
     
-    // save data to the core data
-    func saveUserPreferences() throws{
-        let newUserPreference = UserPreferences(context: context)
-        newUserPreference.fullName = fullName
-        newUserPreference.role = selectedRole
-        newUserPreference.language = selectedLanguage
-        
-        do {
-            try context.save()
-            print("User preferences saved.")
-        } catch {
-            print("Error saving user preferences: \(error.localizedDescription)")
-            throw error
-        }
-
-    }
-    
-    func fetchUserPreferences(){
-        // Fetch the data from core data
-        do{
-            let userPreferences = try context.fetch(UserPreferences.fetchRequest())
-            if let preferences = userPreferences.first {
-                selectedRole = preferences.role ?? ""
-                selectedLanguage = preferences.language ?? ""
-            }
-        } catch {
-            print("Error fetching user preferences: \(error.localizedDescription)")
-        }
-    }
-    
-    private let roles: [String] = [
-        "Passenger",
-        "Driver"
-    ]
-    @State private var selectedRole: String = ""
-    
-    private let languages: [String] = [
-        "English",
-        "Finnish",
-        "Swedish"
-    ]
-    @State private var selectedLanguage: String = ""
-    @State private var fullName = ""
-    @State private var sendNotifications = false
-    @State private var location = false
-    // @State private var aboutUs = ""
-    
+    /*@State private var selectedLanguage: String = ""
+     @State private var fullName = ""
+     @State private var sendNotifications = false
+     @State private var location = false
+     
+     // Reference to managed object context
+     let context = CoreDataManager.shared.persistentContainer.viewContext
+     
+     // Data for the information
+     var items: [UserPreferences]?
+     /*@FetchRequest(entity: UserPreferences.entity(), sortDescriptors: [], animation: .default)
+      var userPreferences: FetchedResults<UserPreferences>*/
+     
+     // save data to the core data
+     func saveUserPreferences() throws{
+     let newUserPreference = UserPreferences(context: self.context)
+     newUserPreference.fullName = fullName
+     newUserPreference.role = selectedRole
+     newUserPreference.language = selectedLanguage
+     
+     do {
+     try context.save()
+     print("User preferences saved.")
+     } catch {
+     print("Error saving user preferences: \(error.localizedDescription)")
+     throw error
+     }
+     
+     }
+     
+     func fetchUserPreferences(){
+     // Fetch the data from core data
+     do{
+     let userPreferences = try context.fetch(UserPreferences.fetchRequest())
+     if let preferences = userPreferences.first {
+     fullName = preferences.fullName ?? ""
+     selectedRole = preferences.role ?? ""
+     selectedLanguage = preferences.language ?? ""
+     }
+     } catch {
+     print("Error fetching user preferences: \(error.localizedDescription)")
+     }
+     }
+     
+     private let roles: [String] = [
+     "Passenger",
+     "Driver"
+     ]
+     @State private var selectedRole: String = ""
+     
+     private let languages: [String] = [
+     "English",
+     "Finnish",
+     "Swedish"
+     ]
+     
+     // @State private var aboutUs = ""
+     */
     var roleSelected: ((String) -> Void)? // callback function
+    
+    init(roleSelected: ((String) -> Void)? = nil) {
+        self.roleSelected = roleSelected
+    }
     
     var body: some View {
         NavigationView{
             List {
                 Section(header: Text("My information")){
-                    TextField("Full Name", text: $fullName)
+                    TextField("Full Name", text: $viewModel.fullName)
                     HStack {
-                        Picker("I am a", selection: $selectedRole) {
-                            ForEach(roles, id: \.self) { role in
+                        Picker("I am a", selection: $viewModel.selectedRole) {
+                            ForEach(viewModel.roles, id: \.self) { role in
                                 Text(role)
+                                    .tag(role)
                             }
                         }
-                        .onChange(of: selectedRole) { value in
+                        .onChange(of: viewModel.selectedRole) { value in
                             roleSelected?(value) // call the callback function when role is selected
                         }
                         .foregroundColor(Color.theme.blue)
                     }
                     HStack {
-                        Picker("Choose a language", selection: $selectedLanguage) {
-                            ForEach(languages, id: \.self) { language in
+                        Picker("Choose a language", selection: $viewModel.selectedLanguage) {
+                            ForEach(viewModel.languages, id: \.self) { language in
                                 Text(language)
                                     .tag(language)
                             }
@@ -94,11 +106,11 @@ struct SettingsView: View {
                     
                 }
                 Section(header:Text ("Notifications")){
-                    Toggle("Notifications", isOn: $sendNotifications)
+                    Toggle("Notifications", isOn: $viewModel.sendNotifications)
                         .foregroundColor(Color.theme.blue)
                 }
                 Section(header:Text ("Location Permission")){
-                    Toggle("Location", isOn: $location)
+                    Toggle("Location", isOn: $viewModel.location)
                         .foregroundColor(Color.theme.blue)
                 }
                 Section(header:Text ("About us")){
@@ -110,16 +122,18 @@ struct SettingsView: View {
                 }
                 Section {
                     Button(action: {
+                        
                         do {
-                            try saveUserPreferences()
+                            try viewModel.saveUserPreferences()
                         } catch {
-                            print("Error saving user preferences: \(error.localizedDescription)")
+                            print("Error deleting user preferences: \(error.localizedDescription)")
                         }                    }) {
                             Text("Save")
                         }
                 }
                 
                 .navigationTitle("Settings")
+                
             }
             
             
